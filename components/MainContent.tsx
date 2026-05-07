ac1028f2609c096d023f1464e73cc1f035fdc32a
Ali <topolovskoda@gmail.com>
2026-05-07 14:09:49 +0100

    optimize bg app

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -1,13 +1,29 @@
 import { getTodos } from "@/src/app/actions/todo/getTodos";
 import NoTasksAdded from "./NoTasksAdded";
 import TodosContent from "./TodosContent";
-import { EditCategoryDialog } from "./EditCategoryDialog";
+import Image from "next/image";
 
 const MainContent = async () => {
   const result = await getTodos();
   const todos = result.data ?? [];
   return (
-    <section className="h-full overflow-y-auto bg-cover rounded-xl duration-200 bg-[url('/dashboard-light.png')] dark:bg-[url('/dashboard-dark.png')]">
+    <section className="relative h-full overflow-y-auto rounded-xl duration-200">
+      <Image
+        src="/dashboard-light.png"
+        alt="Dashboard Background"
+        fill
+        priority // This is the most important prop for LCP!
+        className="object-cover -z-10 dark:hidden"
+        sizes="(max-width: 768px) 100vw, 80vw"
+      />
+      <Image
+        src="/dashboard-dark.png"
+        alt="Dashboard Background"
+        fill
+        priority
+        className="object-cover -z-10 hidden dark:block"
+        sizes="(max-width: 768px) 100vw, 80vw"
+      />
       {todos.length === 0 ? <NoTasksAdded /> : <TodosContent todos={todos} />}
     </section>
   );
